740bcd407c89e11c1709b9e0b9c49f33f113c761
Ayush Khandelwal <ayush@Ayushs-MacBook-Air.local>
2020-07-11 02:52:38 +0530

    tasks could be deleteed

--- app.py
+++ app.py
@@ -26,10 +26,20 @@ def index():
             return redirect('/')
 
         except:
-            return'Error addingg the new task' 
+            return'Error adding the new task' 
     else:
         tasks= Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html',tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete=Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error deleting the new task' 
 
 if __name__ == "__main__":
     app.run(debug=True)
